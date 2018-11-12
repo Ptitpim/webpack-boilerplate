1b4b101281eb67c74a38b12f2b3bf0847eb3b169
François <francois.pouthier@gmail.com>
2018-11-12 15:42:40 +0100

    add babel polyfill support

--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
+import '@babel/polyfill';
+
 const element = document.createElement('h1');
 element.innerText = 'Hello Webpack 4!';
 
 document.body.appendChild(element);
+
+
+// Test for polyfill bundling
+var array = [1,2,3];
+Array.from(array).forEach(($item) => {
+  console.log($item);
+});
